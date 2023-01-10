1728bf49ab449e369cb1cf86b482de9d488938ad
kri-lun <kristianhlundin@gmail.com>
2023-01-10 22:47:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import datetime
 import os
+import webbrowser
 
 import fastf1
 import matplotlib.pylab as plt
-import pandas as pd
 import PySimpleGUI as sg
 from fastf1 import plotting
 import tkinter as tk
@@ -11,16 +11,16 @@ import tkinter as tk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-## Setter opp matplotlib med fastf1. Lar oss plotte timedelta verdier bedre.
-fastf1.plotting.setup_mpl(mpl_timedelta_support=True, color_scheme='fastf1', misc_mpl_mods=True)
+## MatPlotLib config for FastF1
+fastf1.plotting.setup_mpl(mpl_timedelta_support=True, misc_mpl_mods=True)
 
-## Lager directory hvor data kan bli cachet. Dersom filstien ikke eksisterer, lager metoden en ny filsti. 
+## Creates cache directory path
 class CacheDir:
 
-    default = './data/cache'
+    default = 'data/cache'
 
     def __init__(self, path):
-        path = 'path'
+        self.path = 'path'
 
     def Set(path):
         CacheExist = os.path.exists(path)
@@ -28,30 +28,33 @@ class CacheDir:
             os.makedirs(path)
         CacheDir.default = path
 
-fastf1.Cache.enable_cache('./data/cache')
+fastf1.Cache.enable_cache('data/cache')
 
-## Klasse som tar argumentene grandprix og abb(abbreviation/forkortelse for navnet til sjåføren)
-## Henter fører informasjon fra grandprix argument og assigner til "laps" og "driver_info" variabler for gjenbruk.
+
+## Gather info about driver
 class Driver:
     def __init__(self, grandprix, abb):
+        ## Driver abbreviation
         self.id = abb
+        ## Get lap information from driver
         driver_session = grandprix.laps.pick_driver(self.id)
         self.bio = grandprix.get_driver(self.id)
         self.ses = driver_session
         self.tel = driver_session.get_car_data().add_distance()
         self.best = driver_session.pick_fastest()
         self.best_tel = driver_session.pick_fastest().get_car_data().add_distance()
+        ## Color used in line graph
         self.teamcolor = fastf1.plotting.team_color(self.bio['TeamName'])
 
 
 
-## Klasse som lager lister om diverse informasjon som brukeren kan velge å analysere i GUI.
+## Lists of data
 class Lists:
     def __init__(self):
-        self.Years = self.make('Years', list(range(2018, (int(datetime.datetime.today().year)+1))))
+        self.Years = self.make('Years', list(range(2018, (int(datetime.datetime.today().year)))))
         self.GrandPrix = self.make('GrandPrix', list(fastf1.get_event_schedule(self.Years.list[-1])['EventName']))
         self.Sessions = self.make('Sessions', ['FP1', 'FP2', 'FP3', 'Qualifying', 'Race'])
-        self.Drivers = self.make('Drivers', ['Driver 1', 'Driver 2'])
+        self.Drivers = self.make('Drivers', ['Select session'])
         self.DriversComp = self.make('DriversComp', [])
         self.SessionSlice = self.make('SessionSlice', ['Full Session', 'Specific Lap', 'Fastest Lap'])
         self.SesVarsX = self.make('SesVarsX', ['LapNumber'])
@@ -61,7 +64,7 @@ class Lists:
         self.DriverVars = self.make('DriverVars', ['Var 1', 'Var 2'])
         self.Laps = self.make('Laps', [1, 2, 3, 4, 5])
 
-
+    ## Define attributes in "Lists" class
     def make(self, name, _list):
         class Make:
             def __init__(self, name, _list):
@@ -73,8 +76,7 @@ class Lists:
         return Make(name, _list)
         
 
-## Funksjon som lar brukeren bestemme hvilke runder som skal analyseres. 
-## best_tel henter data fra sjåførens beste runde, lap_num tar et user input og henter data fra int inputen til brukeren. Full session henter data fra hele kvalifiseringsøkten.
+## Get lap type
 def set_data(driver, slice, lap_num):
     if slice == "Fastest Lap":
         self = driver.best_tel
@@ -89,70 +91,65 @@ def set_data(driver, slice, lap_num):
 
     return self
 
-## Definerer egenskaper til matplotlib plot som vises i resultat-vinduet. 
-## Bruker subplot funksjon for å kunne plotte og COMPAREe forskjellige førere sin data samtidig i samme figur. 
+ 
 def make_figure():
         fig = plt.figure(1, figsize=(16,9), constrained_layout=True)
+        ## Subplot for better analysis
         ax = fig.subplots()
         return fig, ax
 
+## Subplot data
 def plot_ax(driver, data, figure, xvar, yvar, ax):
     ax.plot(data[xvar], data[yvar], color=driver.teamcolor, label=f'{driver.id}')
     ax.set_xlim(data[xvar].min(), data[xvar].max())
 
-## Plotter data fra data argument til hver akse i subploten. 
-## Setter fargen på linjen til sjåførens lagfarge. Label blir satt til f-string for å importere verdien av driver.id
-## Setter minimum og maksimal grense til x-aksen ved å hente data fra 'data'. Sørger for at all data passer i figuren.  
 
+## Adds new dataset for comparison
 def compare(grandprix, driver, slice, xvar, yvar, fig, ax, lap_num):
     for abb in driver:
         driver = Driver(grandprix, abb)
         data = set_data(driver, slice, lap_num)
         plot_ax(driver, data, fig, xvar, yvar, ax)
 
-## Bestemmer beskrivelse av analyse i tittel over graf. F.eks Fastest lap, Brake, \n 2020 Austrian Grand Prix, Q.
+
+## Figure title
 def set_title(grandprix, driver, yvar, slice, ses, lap_num, comp):
     global title
-    ## Dersom valg av runde er spesifikk runde, blir runde-nr med i tittelen. 
+ 
     if slice == "Specific Lap":
         analysis = f"Lap {lap_num}, {yvar} \n {grandprix.event.year} {grandprix.event['EventName']}, {ses}"
 
-    ## Hvis valg av runde ikke er en spesifikk runde, består tittelen av enten full økt eller raskeste runde.
     elif slice != "Specific Lap":
         analysis = f"{slice}, {yvar} \n {grandprix.event.year} {grandprix.event['EventName']}, {ses}"
 
-    ## Hvis COMPAREing av sjåfører er valgt, gjelder kun regler ovenfor. 
     if comp == True:
         title = analysis
 
-    ## Dersom kun en sjåfør er valgt, vil dens fulle navn vises i tittelen sammen med tidligere regler. 
     elif comp != True:
         title = f"{driver.bio['FullName']} " + analysis
     plt.suptitle(f"{title}")
 
+
 def design_plot(ax, xvar, yvar):
-        ## xvar og yvar representerer hva brukeren vil vise av telemetry. F.eks throttle og distance
+        ## Axes title/label
         ax.set_xlabel(xvar)
         ax.set_ylabel(yvar)
-
-        ## Skrur på minorticks for å gi et bedre bilde av telemetrydata. 
+ 
         ax.minorticks_on()
         ax.grid(visible=True, axis='both', which='major', linewidth=0.9, alpha=0.3)
 
-        ## Endrer minorticks linjestil og linjebredde for å stå ut mindre enn hovedrutene.
         ax.grid(visible=True, axis='both', which='minor', linestyle=':', linewidth=0.6, alpha=0.3)
 
-        ## Viser en oversikt av sjåfør som vises i abb(abbreviation/forkortelse) og hvilken farge som representerer hver sjåfør. 
+        ## Driver info 
         ax.legend()
 
-## Viser graf
+## Plot
 def show_plot():
     plt.show()
 
-##def save_fig():
 
 def analyse():
-    ## Globale input verdier
+    ## Input variables
     global year, gp, ses, abb, slice
     global lap_num, xvar, yvar, comp
 
@@ -166,12 +163,10 @@ def analyse():
     yvar = values['-DRIVERYVAR-']
     comp = values['-COMPARE-']
 
-    ## Lager figur
+
     fig, ax = make_figure()
 
-    ## Sjekk compare status
-    ## Henter sjåfør data for 
-    # Plot Variables
+    ## Check compare checkbox status
     if comp == True:
         compare(grandprix, abb, slice, xvar, yvar, fig, ax, lap_num)
         driver = Driver(grandprix, abb[0])
@@ -185,31 +180,32 @@ def analyse():
     set_title(grandprix, driver, yvar, slice, ses, lap_num, comp)
 
 
-    # Show Plot
+    ## Plot
     show_plot()
-
-
 
 
 def make_window():
     sg.theme('DarkBlue14')
     lists = Lists()
-    menu_def = [[('&Help'), ['&About', 'E&xit']]]
+    menu_def = [[('&Help'), ['&About', 'E&xit', 'GitHub']]]
 
     header_layout = [[sg.Image(source='src/common/images/icon_header.png', size=(120, 60), expand_x=True, expand_y=True)],]
 
 
-    ## Input meny
-    ## Ulike valg for X og Y akse Telemetry for å gjøre brukerens valg mest mulig kompatibel med hverandre. 
+    ## Input menu
     menu_layout_column = [
             [sg.Menubar(menu_def, key='-MENU-')],
-            [sg.Frame('', header_layout, size=(500,100), key='-HEAD-')],
+            [sg.Frame('', header_layout, size=(475,100), key='-HEAD-')],
             [sg.OptionMenu(lists.Years.list, default_value=f'{lists.Years.list[-1]}', expand_x=True, key='-YEAR-')],
             [sg.Button('Load Season', size=(10,1), expand_x=True)],
-            [sg.Listbox(lists.GrandPrix.list, enable_events=True, expand_x=True, size=(None,10), select_mode='single', horizontal_scroll=False, visible=False, pad=(7,7,7,7), key='-GP-')],
+            [sg.Listbox(lists.GrandPrix.list, enable_events=True, expand_x=True, size=(None,8), select_mode='single', horizontal_scroll=False, visible=False, pad=(7,7,7,7), key='-GP-')],
             [sg.OptionMenu(lists.Sessions.list, default_value=f'Select Session...', expand_x=True, visible=False, key='-SESSION-')],
             [sg.Button('Drivers in Session', visible=False, expand_x=True, key='-LOADDRIVERS-')],
-            [sg.Listbox(lists.Drivers.list, enable_events=True, expand_x=True, size=(None,10), select_mode='single', horizontal_scroll=False, visible=False, pad=(7,7,7,7), key='-DRIVER-')],
+            ]
+
+    ## Empty canvas space to draw graph
+    second_menu_layout_column = [
+            [sg.Listbox(lists.Drivers.list, enable_events=True, expand_x=True, size=(40,8), select_mode='single', horizontal_scroll=False, visible=False, pad=(7,7,7,7), key='-DRIVER-')],
             [sg.Checkbox('Compare drivers?', enable_events=True, visible=False, key='-COMPARE-')],
             [sg.OptionMenu(lists.SessionSlice.list, default_value=f'Evalutate Full Session?', disabled=True, expand_x=True, visible=False, key='-SLICE-')],
             [sg.Text('Enter Lap Number', visible=False, key='-LAPASK-')],
@@ -217,19 +213,15 @@ def make_window():
             [sg.Button('Select Telemetry', visible=False, disabled=True, expand_x=True, key='-LOADVARS-')],
             [sg.OptionMenu(lists.LapVarsY.list, default_value='Y Variable...', expand_x=True, visible=False, key='-DRIVERYVAR-')],
             [sg.OptionMenu(lists.LapVarsX.list, default_value='X Variable...', expand_x=True, visible=False, key='-DRIVERXVAR-')],
-            [sg.Button('Confirm All', visible=True, expand_x=True, key='-CONFIRM ALL-')],
-            [sg.Button('Submit', visible=True, disabled=True, expand_x=True, key='-PLOT-')],
-            ]
-
-
-    graph_layout_column = [
-    [sg.Canvas(size=(600, 600), key= '-CANVAS-', background_color='DarkGrey',)]
+            [sg.Button('Confirm All', visible=False, expand_x=True, key='-CONFIRM ALL-')],
+            [sg.Button('Submit', visible=False, disabled=True, expand_x=True, key='-PLOT-')],
     ]
 
+
     layout = [
-        [sg.Column(menu_layout_column),
-        sg.VSeparator(),
-        sg.Column(graph_layout_column)],
+        [sg.Column(menu_layout_column, vertical_alignment='top'),
+        sg.VSeperator(),
+        sg.Column(second_menu_layout_column, vertical_alignment='top')],
                 ]
 
 
@@ -243,7 +235,7 @@ def make_window():
 def main():
     window = make_window()
 
-    # Window Open, Begin Event Loop
+    ## Event Loop
     while True:
         global values
         event, values = window.read(timeout=100)
@@ -251,7 +243,7 @@ def main():
         
         class ButtonFunc:
 
-            #About
+            ## About
             def About():
                 about_layout = [[sg.Text('Gr1dGuru allows you to create simple Formula 1 lap time and telemetry graphs.')],
                                 [sg.Text('Use the selection boxes to highlight your desired data and submit it!')],
@@ -262,7 +254,11 @@ def main():
                     about_win.close()
 
 
-            # Load GPs
+            def GitHub():
+                [webbrowser.open("https://github.com/kri-lun/Gr1dGuru")]
+
+
+            ## Load Grand Prix weekends
             def LoadGPList():
                 lists = Lists()
                 lists.GrandPrix = lists.make('Grand Prix', list(fastf1.get_event_schedule(int(values['-YEAR-']))['EventName']))
@@ -282,7 +278,7 @@ def main():
                 window.refresh()
                 window.read(timeout=100)
 
-            # Load Drivers
+            ## Load Drivers
             def LoadDriverList():
                 lists = Lists()
                 CacheDir.Set(CacheDir.default)
@@ -304,7 +300,7 @@ def main():
                 window.read(timeout=100)
 
             def LoadDriverVars():
-                ## SesVars og LapVars kan kun hentes ved å skape en instance av Lists klassen og loade inn listen ved hjelp av lists istedenfor Lists.
+                ## Get "Lists"
                 lists = Lists()
 
                 if values['-SLICE-'] == 'Full Session':
@@ -333,13 +329,17 @@ def main():
                 window.read(timeout=100)
 
 
-        ##
+        ## Exit
         if event in (None, 'Exit'):
             break
 
         ##
         elif event == 'About':
             ButtonFunc.About()
+
+        ##
+        elif event == 'GitHub':
+            ButtonFunc.GitHub()
 
         ##
         elif event == 'Load Season':
@@ -375,7 +375,7 @@ def main():
         elif event == '-LOADDRIVERS-':
             ButtonFunc.LoadDriverList()
 
-        ## Load Driver VARSs        
+        ## Load telemetry        
         elif event == '-LOADVARS-':
             ButtonFunc.LoadDriverVars()
 
@@ -393,7 +393,7 @@ def main():
     exit(0)
 
 
-## Main funksjon
+## Main 
 if __name__ == '__main__':
     sg.theme('DarkGrey14')
     main()
